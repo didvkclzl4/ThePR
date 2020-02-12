dbeef617bef6a55fc71c6b2d553949356bef6072
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 15:25:26 +0900

    깃허브 연동 테스트

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,16 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("세\n");
+		System.out.print("로\n");
+		System.out.print("드\n");
+		System.out.print("립");
+		
+		//아하 변경되는구나 
+
+	}
+
+}
